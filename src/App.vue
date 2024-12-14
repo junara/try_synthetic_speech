8cba3da0781d706df729d57ef547d8aa21c3bc89
junara <jun5araki@gmail.com>
2024-12-14 20:42:54 +0900

    chore: refactor html

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ import {
   useTimestamp,
 } from '@vueuse/core'
 import VoiceTable from '@/components/VoiceTable.vue'
+import HistoryTable from '@/HistoryTable.vue'
 
 const defaultRate = 1
 const defaultPitch = 1
@@ -118,145 +119,41 @@ const onStop = () => {
 
 <template>
   <header>
-    <div class="wrapper">
-      <h3>Try synthetic speech</h3>
-    </div>
+    <h1>Try synthetic speech</h1>
   </header>
 
   <main>
     <div>
+      <label for="voice">Voice</label>
+      <select id="voice" v-model="voice">
+        <option v-for="v in voices" :key="v.name" :value="v">
+          {{ v.name }}
+        </option>
+      </select>
+      <label for="rate">Rate</label>
+      <input id="rate" v-model="rate" type="number" min="0.1" max="10" step="0.1" />
+      <label for="pitch">Pitch</label>
+      <input id="pitch" v-model="pitch" type="number" min="0.1" max="10" step="0.1" />
+      <label for="text"
+        >Text <i v-if="text.length">{{ text.length }}</i></label
+      >
+      <textarea id="text" v-model="text" placeholder="Type something here" rows="4" cols="30" />
       <div>
-        <div>
-          <label for="voice">Voice</label>
-          <div>
-            <select id="voice" v-model="voice">
-              <option v-for="v in voices" :key="v.name" :value="v">
-                {{ v.name }}
-              </option>
-            </select>
-          </div>
-        </div>
-        <div>
-          <label for="text">Text</label>
-          <div>
-            <textarea
-              id="text"
-              v-model="text"
-              placeholder="Type something here"
-              rows="4"
-              cols="30"
-            />
-            <div>
-              <span>{{ text.length }}</span
-              ><span>character number</span>
-            </div>
-            <div>
-              <span>{{ currentElapsed / 1000 }}</span
-              ><span>sec</span>
-            </div>
-          </div>
-        </div>
-
-        <div>
-          <label for="rate">Rate</label>
-          <input id="rate" v-model="rate" type="number" min="0.1" max="10" step="0.1" />
-          <label for="pitch">Pitch</label>
-          <input id="pitch" v-model="pitch" type="number" min="0.1" max="10" step="0.1" />
-        </div>
+        <button v-if="!isPlaying" :disabled="text.length === 0" @click="onSpeak">Speak</button>
+        <button v-if="isPlaying" @click="onStop">Stop</button>
+        <button v-if="!isPlaying" @click="reset">Reset</button>
       </div>
       <div>
-        <button @click="onSpeak">Speak</button>
-        <button @click="onStop">Stop</button>
-        <button @click="reset">Reset</button>
+        <span>{{ currentElapsed / 1000 }}</span> <span>sec</span>
       </div>
-      <div>
-        <div>
-          <table>
-            <caption>
-              History
-            </caption>
-            <thead>
-              <tr>
-                <th scope="col" role="columnheader">Time</th>
-                <th scope="col" role="columnheader">Status</th>
-                <th scope="col" role="columnheader">Text</th>
-                <th scope="col" role="columnheader">Length</th>
-                <th scope="col" role="columnheader">Voice</th>
-                <th scope="col" role="columnheader">Elapsed</th>
-                <th scope="col" role="columnheader">Rate</th>
-                <th scope="col" role="columnheader">Pitch</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="item in history" :key="item.time">
-                <td role="cell">
-                  {{ item.time }}
-                </td>
-                <td role="cell">
-                  {{ item.status }}
-                </td>
-                <td role="cell">
-                  {{ item.text }}
-                </td>
-                <td role="cell">
-                  {{ item.text.length }}
-                </td>
-                <td role="cell">
-                  {{ item.voice }}
-                </td>
-                <td role="cell">
-                  {{ item.elapsed }}
-                </td>
-                <td role="cell">
-                  {{ item.rate }}
-                </td>
-                <td role="cell">
-                  {{ item.pitch }}
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-
-      <table>
-        <caption>
-          Selected Voice
-        </caption>
-        <thead>
-          <tr>
-            <th scope="col" role="columnheader">Default</th>
-            <th scope="col" role="columnheader">Local Service</th>
-            <th scope="col" role="columnheader">Lang</th>
-            <th scope="col" role="columnheader">URI</th>
-            <th scope="col" role="columnheader">Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td role="cell">
-              {{ voice?.default }}
-            </td>
-            <td role="cell">
-              {{ voice?.localService }}
-            </td>
-            <td role="cell">
-              {{ voice?.lang }}
-            </td>
-            <td role="cell">
-              {{ voice?.voiceURI }}
-            </td>
-            <td role="cell">
-              {{ voice?.name }}
-            </td>
-          </tr>
-        </tbody>
-      </table>
     </div>
-    <div>
+    <figure>
+      <HistoryTable v-if="history.length" :history="history" />
+    </figure>
+    <figure>
       <VoiceTable v-if="voices.length" :voices="voices" />
       <div v-else>Loading...</div>
-    </div>
+    </figure>
   </main>
 </template>
 
---
+++ src/HistoryTable.vue
@@ -0,0 +1,60 @@
+<template>
+  <table>
+    <caption>
+      History
+    </caption>
+    <thead>
+      <tr>
+        <th scope="col" role="columnheader">Time</th>
+        <th scope="col" role="columnheader">Status</th>
+        <th scope="col" role="columnheader">Text</th>
+        <th scope="col" role="columnheader">Length</th>
+        <th scope="col" role="columnheader">Voice</th>
+        <th scope="col" role="columnheader">Elapsed</th>
+        <th scope="col" role="columnheader">Rate</th>
+        <th scope="col" role="columnheader">Pitch</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in history" :key="item.time">
+        <td role="cell">
+          {{ item.time }}
+        </td>
+        <td role="cell">
+          {{ item.status }}
+        </td>
+        <td role="cell">
+          {{ item.text }}
+        </td>
+        <td role="cell">
+          {{ item.text.length }}
+        </td>
+        <td role="cell">
+          {{ item.voice }}
+        </td>
+        <td role="cell">
+          {{ item.elapsed }}
+        </td>
+        <td role="cell">
+          {{ item.rate }}
+        </td>
+        <td role="cell">
+          {{ item.pitch }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script setup lang="ts">
+defineProps<{
+  history: {
+    time: string
+    status: string
+    text: string
+    voice: string
+    elapsed: string
+    rate: number
+    pitch: number
+  }[]
+}>()
+</script>
